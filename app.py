b39a83aa92eeeeb4552e8b6b67f7288ff999612d
DanMitroshin <36602715+DanMitroshin@users.noreply.github.com>
2021-12-09 23:32:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 import solver_dummy
 import solver_gas_dynamics_1d
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.route("/")
+@cross_origin()
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return {'result': 'ok'}
 
 
 @app.route("/solver/<solvername>", methods=["POST"])
+@cross_origin()
 def solver(solvername):
 
     request_json = request.get_json()
+    print(request_json)
 
     if solvername == "dummy":
         response = solver_dummy.handle_request(request_json)
